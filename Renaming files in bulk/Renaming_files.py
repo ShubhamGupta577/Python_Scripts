87e44224c6351a13a8d969a8fea812161d198b46
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-05-07 00:07:32 +0530

    Update Renaming_files.py

--- Renaming files in bulk/Renaming_files.py
+++ Renaming files in bulk/Renaming_files.py
@@ -1,14 +1,22 @@
 import os
+
 def rename():
   i=0
+  #Taking the path input from user
   path=input("Enter the location of directory: ")
   path=path+'/'
+  
+  #Iterating over the files in the directory
   for file in os.listdir(path):
+    #Setting the new name for the file
     name="image"+str(i)+'.jpg'
     source=path+file
     destination=path+destination
+    #Renaming the file name
     os.rename(source,destination)
     i+=1
+    
+#Driver code
 if name=='__main__':
   rename()
   
